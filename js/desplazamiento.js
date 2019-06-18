1244b6d9f33acba39eab1f8d35e4fffa9bae269c
JuanDelgadillo <jsebastian.delgadillo@gmail.com>
2019-06-18 17:17:53 -0400

    Elimino lo comentado

--- js/desplazamiento.js
+++ js/desplazamiento.js
@@ -29,19 +29,3 @@ $(document).ready(function(){
      });   
 
 });
-
-
-// <script>
-//   //Cuando cargue la página completamente
-//   $(document).ready(function(){
-//     //Creamos un evento click para el enlace
-//     $(".ancla").click(function(evento){
-//       //Anulamos la funcionalidad por defecto del evento
-//       evento.preventDefault();
-//       //Creamos el string del enlace ancla
-//       var codigo = "#" + $(this).data("ancla");
-//       //Funcionalidad de scroll lento para el enlace ancla en 3 segundos
-//       $("html,body").animate({scrollTop: $(codigo).offset().top}, 3000);
-//     });
-//   });
-// </script>
